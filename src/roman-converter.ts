0536a166f89a544c244cb88cea7061b6a719070b
Alex <alexmonac13@gmail.com>
2025-10-30 15:53:08 +0100

    green 2 - add symbols from 3 to 10 for covering the new tests

--- src/roman-converter.ts
+++ src/roman-converter.ts
@@ -1,6 +1,6 @@
 export class RomanConverter {
   convert(number: number): string {
-    const romans = ["", "I", "II", "III"];
+    const romans = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
     return romans[number] ?? "";
   }
 }
