0f47c1531d76b03dff4d8eac39a6e566cc895237
Alex <alexmonac13@gmail.com>
2025-10-30 16:07:57 +0100

    red 3 - add the remaining tests to cover every possibility

--- tests/roman-converter.test.ts
+++ tests/roman-converter.test.ts
@@ -2,6 +2,16 @@ import { describe, expect, it } from "vitest";
 import { RomanConverter } from "../src/roman-converter";
 
 describe("Roman Converter Tests", () => {
+  it("should throw an error if number equals 0", () => {
+    const romanConverter = new RomanConverter();
+    expect(() => romanConverter.convert(0)).toThrowError("Number must be between 1 and 3999");
+  });
+
+  it("should throw an error if number is greater than 3999", () => {
+    const romanConverter = new RomanConverter();
+    expect(() => romanConverter.convert(4000)).toThrowError("Number must be between 1 and 3999");
+  });
+
   it("should convert 1 to I", () => {
     const romanConverter = new RomanConverter();
     expect(romanConverter.convert(1)).toBe("I");
@@ -25,5 +35,20 @@ describe("Roman Converter Tests", () => {
   it("should convert 9 to IX", () => {
     const romanConverter = new RomanConverter();
     expect(romanConverter.convert(9)).toBe("IX");
+  });
+
+  it("should convert 58 to LVIII", () => {
+    const romanConverter = new RomanConverter();
+    expect(romanConverter.convert(58)).toBe("LVIII");
+  });
+
+  it("should convert 1994 to MCMXCIV", () => {
+    const romanConverter = new RomanConverter();
+    expect(romanConverter.convert(1994)).toBe("MCMXCIV");
+  });
+
+  it("should convert 3999 to MMMCMXCIX", () => {
+    const romanConverter = new RomanConverter();
+    expect(romanConverter.convert(3999)).toBe("MMMCMXCIX");
   });
 });
