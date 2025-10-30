35996eaf59718477bfb0b6dd58912e88465d7f6c
Alex <alexmonac13@gmail.com>
2025-10-30 15:54:58 +0100

    refacto 2 - add an correspondency array and a better converter logic for covering the most of cases

--- src/roman-converter.ts
+++ src/roman-converter.ts
@@ -1,6 +1,22 @@
 export class RomanConverter {
-  convert(number: number): string {
-    const romans = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
-    return romans[number] ?? "";
+  private readonly romanNumerals: Array<{ value: number; symbol: string }> = [
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" },
+  ];
+
+  convert(number: number) {
+    let result = "";
+
+    for (const { value, symbol } of this.romanNumerals) {
+      while (number >= value) {
+        result += symbol;
+        number -= value;
+      }
+    }
+
+    return result;
   }
 }
